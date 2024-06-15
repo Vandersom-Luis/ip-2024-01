e8c3ea967cff42c717d8bd2cb0e086e063c57efb
Vandersom-Luis <vandersom.silva@discente.ufg.br>
2024-06-14 21:12:56 -0300

    ok

---
+++ Aula 15/Questão 1/questao1.go
@@ -0,0 +1,14 @@
+// Questão 1
+package main
+import "fmt"
+
+func elevado(x, n int) int {
+    if n <= 0 {
+        return 1
+    }
+    return x * elevado(x, n - 1)
+}
+
+func main() {
+  fmt.Print(elevado(2, 3))
+}
---
+++ Aula 15/Questão 2/questao2.go
@@ -0,0 +1,23 @@
+// Questão 2
+package main
+import "fmt"
+
+func remove(slice []int, s int) []int {
+    return append(slice[:s], slice[s+1:]...)
+}
+
+func soma(array []int) int {
+    if len(array) == 1 {
+        return array[0]
+    }
+
+    return array[len(array) - 1] + soma(remove(array, len(array) - 1))
+}
+
+
+
+func main() {
+    array := []int{1, 2, 3, 4} 
+
+    fmt.Print(soma(array))
+}
